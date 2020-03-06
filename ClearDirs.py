4c720bd906cc9a54aa21adab9eb155110d997b01
Michael Hatmaker <Michael.Hatmaker@gmail.com>
2020-03-06 13:43:33 -0700

    Adds clear directories capability to ClearDirs.py to clean up hard drive

--- ClearDirs.py
+++ ClearDirs.py
@@ -1,12 +1,11 @@
 # remove unused directories
-import os, shutil
+import os
 import argparse
 import pdb
 
 class DirCleaner():
     def __init__(self, rootdir):
         self.rootdir = rootdir
-        self.bkupdir = ''
 
     def checkDirectories(self):
         # pdb.qrace()
@@ -18,31 +17,26 @@ class DirCleaner():
             if not files:
                 print(dirpath, 'is empty')
 
-    def startBackup(self, bkupdir):
-        print("copy files from {0}".format(self.rootdir))
-        print("to destination {0}".format(bkupdir))
-        self.bkupdir = bkupdir
+    def cleanSubDirectories(self, path):
+        files = os.listdir(path)
+        if len(files):
+            for f in files:     # date subdirectories for podcast, plus latest.xml file
+                fullpath = os.path.join(path, f)
+                if os.path.isdir(fullpath):        # date subdirectory
+                    for fn in os.listdir(fullpath):
+                        if fn.endswith('.mp3'):
+                            os.remove(os.path.join(fullpath, fn))
+                    os.rmdir(fullpath)
 
-        # if(os.path.isdir(bkupdir) == False):
-        #     os.mkdir(bkupdir)
-        self.backup(bkupdir)
 
-    def backup(self, path):    NEED TO LOSE RECURSION
-        for dirpath, dirnames, files in os.walk(self.rootdir):
-            # pdb.set_trace()
-            if files:
-                print(dirpath, 'has files')
-                print("number of files in {0} is {1}".format(path, len(files)))
-            if len(files):
-                for f in files:
-                    fullpath = os.path.join(path, f)
-                    if os.path.isdir(fullpath):
-                        self.backup(fullpath)
-                    else:
-                        shutil.copytree(path, self.bkupdir)
-
-            if not files:
-                print
+    def cleanDirectories(self):
+        rtdir = self.rootdir
+        files = os.listdir(rtdir)
+        if len(files):
+            for f in files:              # podcast directories with date subdirectories
+                fullpath = os.path.join(rtdir, f)
+                if os.path.isdir(fullpath):
+                    self.cleanSubDirectories(fullpath)
 
 
     def removeEmptyFolders(self, path, removeRoot=True):
@@ -75,18 +69,24 @@ def startCleanup(args) :
     if(args['startdir']):
         rootdir = args['startdir']
     cleaner = DirCleaner(rootdir)
-    if(args['backup']):
-        bkupdir = args['backup']
-        cleaner.startBackup(bkupdir)
-    else:
+    if(args['remove'] == True)  :
         cleaner.removeEmptyFolders(rootdir, True)
+    elif(args['clean'] == True) :
+        cleaner.cleanDirectories()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Description of your program')
     parser.add_argument('-d','--startdir', help='Specify a source directory', required=False)
-    parser.add_argument('-c', '--backup', help='Specify a copy destination directory', required=False)
+    parser.add_argument('-c', '--clean', help="Clean intermediate directories", default=False, action='store_true')
+    parser.add_argument('-r', '--remove', help="Remove only empty directories", default=False, action='store_true')
 
     # pdb.set_trace()
     args = vars(parser.parse_args())
-    startCleanup(args)
+    if(args['clean']) :
+        if(args['clean'] == True) :
+            startCleanup(args)
+
+    if(args['remove']) :
+        if(args['remove'] == True) :
+            startCleanup(args)
