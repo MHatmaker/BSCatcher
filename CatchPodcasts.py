acf13286cc52d1e43a77e610293c183a30fdf51c
MHatmaker <Michael.Hatmaker@gmail.com>
2020-07-02 14:39:28 -0700

    adds try/except for planet money filename too long

--- CatchPodcasts.py
+++ CatchPodcasts.py
@@ -225,28 +225,33 @@ class CatchPodcasts():
             print("urlopen {0}".format(url))
             u = urllib.request.urlopen(url['srcUrl'])
 
-            f = open(file_name, 'wb')
-            meta = u.info()
-            # pdb.set_trace()
-            file_size = self.findContentLength(meta.items())
-            # file_size = int(meta.items()[1][1])   # ("Content-Length")[0])
-            print(">>>>>>>>>> Downloading: %s Bytes: %s" % (file_name, file_size))
-            downloaded.append(file_name)
+            try:
+                f = open(file_name, 'wb')
+                meta = u.info()
+                # pdb.set_trace()
+                file_size = self.findContentLength(meta.items())
+                # file_size = int(meta.items()[1][1])   # ("Content-Length")[0])
+                print(">>>>>>>>>> Downloading: %s Bytes: %s" % (file_name, file_size))
+                downloaded.append(file_name)
 
-            file_size_dl = 0
-            block_sz = 8192
-            while True:
-                buffer = u.read(block_sz)
-                if not buffer:
-                    break
+                file_size_dl = 0
+                block_sz = 8192
+                # pdb.set_trace()
+                while True:
+                    buffer = u.read(block_sz)
+                    if not buffer:
+                        break
 
-                file_size_dl += len(buffer)
-                f.write(buffer)
-                status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-                status = status + chr(8)*(len(status)+1)
-                # print(status)
+                    file_size_dl += len(buffer)
+                    f.write(buffer)
+                    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+                    status = status + chr(8)*(len(status)+1)
+                    # print(status)
 
-            f.close()
+                f.close()
+            except:
+                print("file name too long or some other file open problem")
+
 
 
     def parseXml(self):
